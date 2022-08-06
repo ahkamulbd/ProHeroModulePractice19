232c1c5bacdea885de279189ee604816b5e62110
ahkamulbd <ahkamulbd@gmail.com>
2022-08-06 11:40:15 +0600

    Uses of switch in place of uses of if-else addressed according to Lecture 19-9

--- module19-6Object.js
+++ module19-6Object.js
@@ -1,5 +1,5 @@
 var student = {
-    id: 115;
+    id: 115,
     name: 'Solaiman Khan',
     class: 9,
     marks: 98
@@ -13,7 +13,7 @@ var mobile = {
 }
 
 var myComputer = {
-    brand: 'HP', j
+    brand: 'HP',
     price: 39000,
     color: 'silver',
     processor: 'i7'
---
+++ module19-9.js
@@ -0,0 +1,22 @@
+// Alternative of if-else / Using switch
+
+var color = 'magenta';
+switch (color) {
+    case 'green':
+        console.log('You are a green friend.')
+        break;
+    case 'blue':
+        console.log('You are a blue friend.')
+        break;
+    case 'white':
+        console.log('You are a white friend.')
+        break;
+    case 'red':
+        console.log('You are a red friend.')
+        break;
+    case 'yellow':
+        console.log('You are a yellow friend.')
+        break;
+    default:
+        console.log('You are my kala kala friend.')
+}
